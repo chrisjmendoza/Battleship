a26e6986e4a54af5d8d288d378989ff3151b40f8
Chris Mendoza <chrisjmendoza@gmail.com>
2015-11-09 12:34:13 -0800

    Create ShipType.java
    Let's discuss what else we ned to add to this if anything.

---
+++ ShipType.java
@@ -0,0 +1,42 @@
+/**
+ * The abstract ship class that will be used to define each ship
+ * After writing some stuff for this, since each method doesn't need to be different for each ship, maybe we don't need an abstract class
+ *
+ * @author Casey Riggin
+ * @author Christopher Mendoza
+ * @author Peter Kim
+ * @author Sai Chang
+ */
+
+public class Ship {
+
+int length;
+String type;
+String direction;
+String origin; // We need to store the origin somewhere in here, how do we want to accomplish that?
+
+public Ship() {
+  
+}
+
+public Ship(int length, String type, String direction, String origin) {
+this.length = length;
+this.type = type;
+this.direction = direction;
+this.origin = origin;
+}
+
+// method to retrieve it's orginiating location
+public String getOrigin() {
+return origin;
+}
+
+// method to retrieve it's length
+public int getLength() {
+return length;
+}
+
+// method to retrieve it's direction
+public String getDirection();
+return direction;
+}
