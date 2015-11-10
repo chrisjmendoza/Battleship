53744bccbc9b9ed1e193c115a59320b69f94e837
Chris Mendoza <chrisjmendoza@gmail.com>
2015-11-09 23:04:59 -0800

    Update IBattleshipModel.java

--- IBattleshipModel.java
+++ IBattleshipModel.java
@@ -129,23 +129,6 @@ public interface IBattleshipModel {
 }
 
 /**
- * A helper class storing a ships type, length, and damage level
- */
-public interface IShip {
-	/**
-	 * Get the type of the ship that this instance is.
-	 * @return ShipType get the Ship. 
-	 */
-	public ShipType getShipType(); 
-	
-	/**
-	 * 
-	 * @return a list of cells the board is consuming with the letter (i.e. B7)
-	 */
-	public String[] getConsumingCells();
-}
-
-/**
  * Defense tile status calls
  */
 enum DefenseTileStatus {
