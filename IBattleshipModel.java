082c589733f9e9fd47901deeb940f216a4ea9083
Casey Riggin <krash.kon@gmail.com>
2015-11-16 19:12:30 -0800

    Remove col and row headers of some methods because they are redundant

--- IBattleshipModel.java
+++ IBattleshipModel.java
@@ -43,23 +43,19 @@
  	
  	/**
  	 * Places Ship at the given location
- 	 * @param col column position to place ship
- 	 * @param row row position to place the ship
  	 * @param ship the type of ship being placed, used to test validity of input
  	 * @param player true for player 1, false for player 2
  	 * @throws IllegalArgumentException if same arguments return false for isValidShipPlacement method
  	 * @throws IllegalStateException is not in setup mode
  	 */
- 	public void placeShip(int col, char row, IShip ship, boolean player);
+ 	public void placeShip (IShip ship, boolean player);
  	
  	/**
  	 * Method checks that placement of the given ship is valid by meeting the following conditions:
- 	 * 1.	That the column is between A-K (inclusive)
- 	 * 2.	That the row is bewtween 1-10 (inclusive)
- 	 * 3.	That the ship does not overlap with any ships by sharing cells
- 	 * 4.	That all of the peices of a ship are on the board
- 	 * 5.	That the ships do not overlap in an diagnoal fashion
- 	 * 6.	That the allowed amount of that ship type have not been already placed by
+ 	 * 1.	That the ship does not overlap with any ships by sharing cells
+ 	 * 2.	That all of the peices of a ship are on the board
+ 	 * 3.	That the ships do not overlap in an diagnoal fashion
+ 	 * 4.	That the allowed amount of that ship type have not been already placed by
  	 * that player.
  	 * 
  	 * @param col column position to place ship
@@ -69,7 +65,7 @@
  	 * @throws IllegalStateException is not in setup mode
  	 * @returns true if placement is valid
  	 */
- 	public boolean isValidShipPlacement(int col, char row, IShip ship, boolean player);
+ 	public boolean isValidShipPlacement(IShip ship, boolean player);
  	
  	/**
  	 * Sends an attack to given position. If the attack returns a miss, switch the active turn
