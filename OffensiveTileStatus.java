b887e8b3f08436e4a22a7739fcc5c62a3e6b9786
Casey Riggin <krash.kon@gmail.com>
2015-11-18 18:29:45 -0800

    Create OffensiveTileStatus.java

---
+++ OffensiveTileStatus.java
@@ -0,0 +1,14 @@
+/**
+ * Offensive tile status calls
+ */
+public enum OffensiveTileStatus
+{
+ 	UNKNOWN,
+ 	MISS,
+ 	HIT_SHIP,
+ 	SUNK_DESTOYER,
+ 	SUNK_CRUISER,
+ 	SUNK_BATTLESHIP,
+ 	SUNK_CARRIER
+}
+ 
