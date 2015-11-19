6017e4b7ddde62407a8158acf8d8a13af19c87f2
Chris Mendoza <chrisjmendoza@gmail.com>
2015-11-18 18:36:11 -0800

    Update Ship.java
    Made some minor updates to ship class.

--- Ship.java
+++ Ship.java
@@ -15,28 +15,48 @@ String type;
 String direction;
 String origin; // We need to store the origin somewhere in here, how do we want to accomplish that?
 
+/**
+ * Default constructor, does pretty much nothing
+ */
 public Ship() {
-  
+  // DO SOME NULL HERE?
+  length = null;
+  type = "";
+  direction = "";
+  origin = "";
 }
 
-public Ship(int length, String type, String direction, String origin) {
+/**
+ * Ship constructor
+ * @param length The length of the ship
+ * @param type  The type of ship (eg destroyer, etc)
+ * @param origin  The starting grid point of the ship
+ * @param direction The direction the ship is facing from the origin (N, NE, E, SE)
+ */
+public Ship(int length, String type, String origin, String direction) {
 this.length = length;
 this.type = type;
 this.direction = direction;
 this.origin = origin;
 }
 
-// method to retrieve it's orginiating location
+/**
+ * Getter for ship origin
+ */
 public String getOrigin() {
-return origin;
+  return origin;
 }
 
-// method to retrieve it's length
+/**
+ * Getter for ship length
+ */
 public int getLength() {
-return length;
+  return length;
 }
 
-// method to retrieve it's direction
+/**
+ * Getter for ship's direction
+ */
 public String getDirection();
 return direction;
 }
